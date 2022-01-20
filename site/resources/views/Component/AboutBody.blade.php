5cdef59e21b35ee64b787212d24ffc6d56fba567
RehanAkram331 <rehanakram331@gmail.com>
2022-01-20 18:40:38 +0600

    rehan
    rehan

--- site/resources/views/Component/AboutBody.blade.php
+++ site/resources/views/Component/AboutBody.blade.php
@@ -1,19 +1,10 @@
 <div class="container-fluid section-marginTop text-center">
     <div class="row ">
-        <div class="col-md-6 contact-form border-dark">
+        <div class="col-md-12 contact-form border-dark">
 
             <img src="{{url('/images/rehan.jpg')}}" class="img-fluid" alt="Wild Landscape" />
 
 
         </div>
-        <div class="col-md-6 contact-form">
-            <h5 class="service-card-title">নাম: রেহান আকরাম</h5>
-            <button id="aaaaaa">dsf</button>
-
-
-
-
-
-        ></div>
     </div>
 </div>
